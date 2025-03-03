811f58da18e6d06499087a1d05cf287493944a9f
Seigo <146911115+YamadaSeigo@users.noreply.github.com>
2025-03-03 23:20:14 +0900

    Test Push
    メッセージボックス表示

--- DX3D11TestApp/DX3D11TestApp/src/main.cpp
+++ DX3D11TestApp/DX3D11TestApp/src/main.cpp
@@ -1,5 +1,9 @@
 
+
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
+	MessageBoxA(NULL, "Hello Windows API", "Message", MB_OK);
+
 	return 0;
 }
